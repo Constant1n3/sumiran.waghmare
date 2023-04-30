912c873c8d97698e0e02d583358ddac29204bd7c
Sumiran Waghmare <38437305+Constant1n3@users.noreply.github.com>
2023-04-30 18:52:17 +0530

    Added routing example and a screen to show a table

--- intro_page/lib/main.dart
+++ intro_page/lib/main.dart
@@ -1,6 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() async {
+  // Avoid errors caused by flutter upgrade.
+  // Importing 'package:flutter/widgets.dart' is required.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Open the database and store the reference.
+  final database = openDatabase(
+    // Set the path to the database. Note: Using the `join` function from the
+    // `path` package is best practice to ensure the path is correctly
+    // constructed for each platform.
+    join(await getDatabasesPath(), 'commodities_database.db'),
+    // When the database is first created, create a table to store dogs.
+    onCreate: (db, version) {
+      // Run the CREATE TABLE statement on the database.
+      return db.execute(
+        'CREATE TABLE commodity(id INTEGER PRIMARY KEY, name TEXT, quantity TEXT, useby TEXT, itemcount INTEGER, cost INTEGER)',
+      );
+    },
+    // Set the version. This executes the onCreate function and provides a
+    // path to perform database upgrades and downgrades.
+    version: 1,
+  );
+
+  // Define a function that inserts dogs into the database
+  Future<void> insertCommodity(Commodity commodity) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Insert the Dog into the correct table. You might also specify the
+    // `conflictAlgorithm` to use in case the same dog is inserted twice.
+    //
+    // In this case, replace any previous data.
+    await db.insert(
+      'commodities',
+      commodity.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  var ketchup = const Commodity(
+    id: 0,
+    name: 'Ketchup',
+    quantity: '35',
+    useby: '22/07/24',
+    itemcount: 10,
+    cost: 2,
+  );
+
+  await insertCommodity(ketchup);
+
+  // A method that retrieves all the dogs from the dogs table.
+  Future<List<Commodity>> commodities() async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Query the table for all The Dogs.
+    final List<Map<String, dynamic>> maps = await db.query('commodities');
+
+    // Convert the List<Map<String, dynamic> into a List<Dog>.
+    return List.generate(maps.length, (i) {
+      return Commodity(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        quantity: maps[i]['quantity'],
+        useby: maps[i]['useby'],
+        itemcount: maps[i]['itemcount'],
+        cost: maps[i]['cost'],
+      );
+    });
+  }
+
+  Future<void> updateCommodity(Commodity commodity) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Update the given Dog.
+    await db.update(
+      'commodities',
+      commodity.toMap(),
+      // Ensure that the Dog has a matching id.
+      where: 'id = ?',
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [commodity.id],
+    );
+  }
+
+/*
+// Update Fido's age and save it to the database.
+fido = Dog(
+  id: fido.id,
+  name: fido.name,
+  age: fido.age + 7,
+);
+await updateDog(fido);
+
+// Print the updated results.
+print(await dogs()); // Prints Fido with age 42.
+*/
+
+  Future<void> deleteCommodity(int id) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Remove the Dog from the database.
+    await db.delete(
+      'commodities',
+      // Use a `where` clause to delete a specific dog.
+      where: 'id = ?',
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [id],
+    );
+  }
+
   runApp(const MyApp());
 }
 
@@ -135,9 +251,14 @@ class _MyHomePageState extends State<MyHomePage> {
               title: const Text('Item 1'),
               onTap: () {
                 // Update the state of the app
-                // ...
-                // Then close the drawer
                 Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondRoute()),
+                );
+                // Then close the drawer
+
+                //Navigator.pop(context);
               },
             ),
             ListTile(
@@ -159,5 +280,113 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+//Example to route to another screen
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: DBHelper().getData(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          List<Map<String, dynamic>> data =
+              snapshot.data as List<Map<String, dynamic>>;
+          return DataTable(
+            columns: [
+              DataColumn(label: Text("ID")),
+              DataColumn(label: Text("Name")),
+            ],
+            rows: data
+                .map((e) => DataRow(cells: [
+                      DataCell(Text(e["id"].toString())),
+                      DataCell(Text(e["name"])),
+                    ]))
+                .toList(),
+          );
+        }
+      },
+    );
+  }
+}
+
+//DBHelper to run DB CRUD
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+
+  factory DBHelper() => _instance;
+
+  DBHelper._internal();
+
+  static Database? _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await initDb();
+    return _db!;
+  }
+
+  initDb() async {
+    String path = join(await getDatabasesPath(), "commodities.db");
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  void _onCreate(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE commodity(id INTEGER PRIMARY KEY, name TEXT, quantity TEXT, useby TEXT, itemcount INTEGER, cost INTEGER)");
+  }
+
+  Future<List<Map<String, dynamic>>> getData() async {
+    var dbClient = await db;
+    var result = await dbClient.query("commodity");
+    return result;
+  }
+}
+
+//storage model for the SQLite DB
+class Commodity {
+  final int id;
+  final String name;
+  final String quantity;
+  final String useby;
+  final int itemcount;
+  final int cost;
+
+  const Commodity({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.useby,
+    required this.itemcount,
+    required this.cost,
+  });
+
+  // Convert a Dog into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'quantity': quantity,
+      'useby': useby,
+      'itemcount': itemcount,
+      'cost': cost,
+    };
+  }
+
+  // Implement toString to make it easier to see information about
+  // each dog when using the print statement.
+  @override
+  String toString() {
+    return 'Dog{id: $id, name: $name, quantity: $quantity, useby:$useby, itemcost:$itemcount, cost:$cost}';
   }
 }
